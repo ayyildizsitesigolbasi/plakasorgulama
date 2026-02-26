20698d6c83fb60675061c7af3028c624c97baf72
ayyildizsitesigolbasi <ayyildizsitesigolbasi@gmail.com>
2026-02-26 15:47:26 +0300

    sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = "plaka-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./data.csv",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => k !== CACHE ? caches.delete(k) : null)
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) =>
+      cached || fetch(e.request)
+    )
+  );
+});
